047172f8392c8fc6bfad93dba478b32f3ed25bd9
Yibing Wang <y.wang@hollandptc.nl>
2025-02-25 16:11:24 +0100

    rabbitmq added

--- dcelery/dcelery/settings.py
+++ dcelery/dcelery/settings.py
@@ -103,5 +103,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+# CELERY_BROKER_URL = os.environ.get("CELERY_BROKER", "redis://redis:6379/0")
+CELERY_BROKER_URL = os.environ.get(
+    "CELERY_BROKER", "amqp://guest:guest@rabbitmq:5672//"
+)
 CELERY_RESULT_BACKEND = os.environ.get("CELERY_BACKEND", "redis://redis:6379/0")
--- dcelery/newapp/tasks.py
+++ dcelery/newapp/tasks.py
@@ -1,13 +1,30 @@
+from time import sleep
 from celery import shared_task
 
 
 @shared_task
-def task1():
+def tp1(queue="celery"):
+    sleep(3)
     print("Task 1")
-    return
+    return "Task 1"
 
 
 @shared_task
-def task2():
+def tp2(queue="celery:1"):
+    sleep(3)
     print("Task 2")
-    return
+    return "Task 2"
+
+
+@shared_task
+def tp3(queue="celery:2"):
+    sleep(3)
+    print("Task 3")
+    return "Task 3"
+
+
+@shared_task
+def tp4(queue="celery:3"):
+    sleep(3)
+    print("Task 4")
+    return "Task 4"
